c9484268b63bba2b69d6716f69faaa7dbd4a44cc
Tasil <christoph.jurkun@edu.techstarter.de>
2024-06-10 11:50:48 +0200

    Verbindung Mongo

--- server/app.js
+++ server/app.js
@@ -9,6 +9,19 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Set up mongoose connection
+const mongoose = require("mongoose"); //  Dies importiert das mongoose-Modul, das eine objektorientierte Modellierung für MongoDB in Node.js ermöglicht.
+mongoose.set("strictQuery", false); // Diese Zeile setzt die Mongoose-Einstellung "strictQuery" auf false. Diese Einstellung beeinflusst das Verhalten von Mongoose beim Verarbeiten von Abfragen.
+
+const dev_db_url =
+  "mongodb+srv://christoph.jurkun:BWCkoSUW3jfpsMQ0@kartenworld.oagdx46.mongodb.net/?retryWrites=true";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+main().catch((err) => console.log(err)); // Diese Zeile ruft die main-Funktion auf und fängt Fehler ab, die während der Ausführung auftreten können.
+async function main() { // Dies definiert eine asynchrone Funktion namens main, die verwendet wird, um den Hauptteil deiner Anwendung auszuführen
+  await mongoose.connect(mongoDB); // In dieser Zeile wird eine Verbindung zur MongoDB-Datenbank hergestellt. mongoose.connect() ist eine Funktion von Mongoose, die eine Verbindung zur MongoDB-Datenbank herstellt.
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
