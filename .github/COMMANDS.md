1e0df530c30cc9c83375fc0b75d501094ef12fb4
Roman Hanzlik <smatplacid@gmail.com>
2024-06-11 18:27:55 +0200

    package und depencies

---
+++ .github/COMMANDS.md
@@ -0,0 +1,10 @@
+## npm commands
+
+
+| Befehl      | Wirkung |
+| ----------- | ----------- |
+| **npm install -g npm-check**      | zuerst den npm-check installieren |
+| **npm audit fix --force**         | integrierter updater |
+| **npm-check**                     | check depencies for latest version |
+| **npm-check -u**                  | interactive updater |
+| **npm -v**                        | get npm version |
---
+++ client/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // Passe den Pfad zu deinem Haupteinstiegspunkt an
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader',
+      },
+      // andere Loader können hier hinzugefügt werden
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'], // Erlaubt das Weglassen der Dateierweiterungen bei Importen
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+  },
+};
